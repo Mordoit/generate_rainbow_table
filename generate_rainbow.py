3e3b22ab607a74e73e39c8fb6de8b15cbef688dc
Mordoit <52674315+Mordoit@users.noreply.github.com>
2019-07-08 20:06:56 +0200

    Add files via upload

---
+++ generate_rainbow.py
@@ -0,0 +1,48 @@
+# /usr/bin/python3
+
+# Introduction : Generate rainbow table with different algo
+# Authors : Troyan
+
+# importe used library
+import hashlib
+import sys
+import os
+
+# Print the error and exit the program
+def error():
+    print("generate_rainbow.py\n-------------------\nAuthor: Troyan \n \nalgorithm supported is the same than the hashlib \nsha1, md5, sha224, sha256, sha384, sha512, blake2b, blake2s, sha3_224, sha3_256, sha3_384, sha3_512 \ngenerate_rainbow.py 'algorithmm' 'path' \n\nExemple: generate_rainbow.py md5 ./password.txt")
+    exit()
+
+if sys.argv[1] == "help" or sys.argv[2] == "help":
+    error()
+
+# Set parameter as variable
+algo = sys.argv[1]
+file = sys.argv[2]
+
+# Generate the name of the output
+output = algo + "_rainbow.txt"
+
+# Controler
+def main():
+    check()
+    rainbow()
+            
+# Check if all the parameter are correct and supported
+def check():
+    exists = os.path.isfile(file)
+    support_Arg = ["sha1", "md5", "sha224", "sha256", "sha384", "sha512", "blake2b", "blake2s", "sha3_224", "sha3_256", "sha3_384", "sha3_512"]
+    if algo not in support_Arg and not exists:
+        error()
+
+# generate the rainbow table
+def rainbow():
+    with open(output, "w") as out:
+        with open(file) as f:
+            test = "out.write(hashlib." + algo + "(line.encode('utf-8', errors='ignore')).hexdigest() + '\t' + line)"
+            for line in f:
+                exec(test)
+
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,15 @@
+# Introduction
+This script is make for create rainbow table of all the password on a file
+
+# Requirement
+The input file has to be named "password.txt" each different password has to be on a different line
+
+# Option
+-a algorith to use (default md5)
+-f file as input (default ./password.txt)
+
+## Availible
+md5, SHA1, 
+
+# Exemple
+python3 generate_rainbow.py -a md5
